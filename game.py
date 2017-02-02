ae8c0bffe79981d618b4b735245a2c9afa7563c7
Zachary Stevens <zed2808@gmail.com>
2017-02-01 22:43:57 -0500

    Fix pip counting, spacing

--- game.py
+++ game.py
@@ -17,14 +17,14 @@ class Game:
 				self.gs.hands[player].add_card(self.gs.deck.draw())
 
 		# Print player's hand
-		print('Your hand:\n{}'.format(self.gs.hands[0].to_string()))
+		print('\nYour hand:\n{}'.format(self.gs.hands[0].to_string()))
 
 		# Do bidding round, return highest bidder
 		self.bidding_round()
 		self.gs.active_player = self.gs.bidder
 
 		# Print highest bidder and their bid
-		print('Player {} won the bidding round with a bid of {}.'.format(self.gs.bidder + 1, self.gs.bid))
+		print('\nPlayer {} won the bidding round with a bid of {}.'.format(self.gs.bidder + 1, self.gs.bid))
 
 		# Set round from -1 (bidding round) to 0
 		self.gs.round = 0
@@ -36,7 +36,7 @@ class Game:
 
 		# Print each player's tricks after the hand
 		for player in range(self.gs.num_players):
-			print('Player {} took {}'.format(player + 1, self.gs.tricks[player].to_string()))
+			print('\nPlayer {} took {}'.format(player + 1, self.gs.tricks[player].to_string()))
 
 		# Score each player's hand and increment their score accordingly
 		self.score_hands()
@@ -48,7 +48,7 @@ class Game:
 
 	# Returns player that wins the bidding round
 	def bidding_round(self):
-		print('Player {} bids first.'.format(self.gs.active_player + 1))
+		print('\nPlayer {} bids first.'.format(self.gs.active_player + 1))
 
 		bid = 0
 
@@ -78,18 +78,18 @@ class Game:
 				bid = self.pb.bid(self.gs)
 				# Bot passes
 				if bid == 0:
-					print('Player {} passes the bid.'.format(self.gs.active_player + 1))
+					print('\nPlayer {} passes the bid.'.format(self.gs.active_player + 1))
 				else:
 					self.gs.bid = bid
 					self.gs.min_bid = bid + 1
 					self.gs.bidder = self.gs.active_player
-					print('Player {} bid {}.'.format(self.gs.active_player + 1, bid))
+					print('\nPlayer {} bid {}.'.format(self.gs.active_player + 1, bid))
 
 			self.gs.next_player()
 
 		# No bids, dealer's bid is forced
 		if self.gs.bid < 2:
-			print('Player {} is forced to bid {}.'.format(self.gs.active_player + 1, self.gs.min_bid))
+			print('\nPlayer {} is forced to bid {}.'.format(self.gs.active_player + 1, self.gs.min_bid))
 			self.gs.bid = self.gs.min_bid
 			self.gs.bidder = self.gs.active_player
 
@@ -98,7 +98,7 @@ class Game:
 		# Give dealer chance to match or pass
 		# Human
 		if self.gs.active_player == 0:
-			print('As the dealer, you may match Player {}\'s bid of {} or pass.'.format(self.gs.active_player + 1, self.gs.bid))
+			print('\nAs the dealer, you may match Player {}\'s bid of {} or pass.'.format(self.gs.active_player + 1, self.gs.bid))
 			# Loop until valid input (match/pass)
 			while True:
 				bid = input('Match bid or pass (0): ')
@@ -113,15 +113,15 @@ class Game:
 
 		# Dealer passes
 		if bid == 0:
-			print('Player {} passes.'.format(self.gs.active_player + 1))
+			print('\nPlayer {} passes.'.format(self.gs.active_player + 1))
 		# Dealer matches
 		else:
-			print('Player {} matches Player {}\'s bet of {}.'.format(self.gs.active_player + 1, self.gs.bidder + 1, self.gs.bid))
+			print('\nPlayer {} matches Player {}\'s bet of {}.'.format(self.gs.active_player + 1, self.gs.bidder + 1, self.gs.bid))
 			self.gs.bidder = self.gs.active_player
 
 	# Play a round and return the winning player
 	def do_round(self):
-		print('Player {} is leading out.'.format(self.gs.active_player + 1))
+		print('\nPlayer {} is leading out.'.format(self.gs.active_player + 1))
 
 		# Give each player a turn to play their card
 		for turn in range(self.gs.num_players):
@@ -134,7 +134,7 @@ class Game:
 				# Human
 				if self.gs.active_player == 0:
 					# Print player's hand
-					print('Your hand:\n{}'.format(self.gs.hands[self.gs.active_player].to_string()))
+					print('\nYour hand:\n{}'.format(self.gs.hands[self.gs.active_player].to_string()))
 					# Print selection numbers under hand
 					for i in range(self.gs.hands[self.gs.active_player].size()):
 						print('{}. '.format(i + 1), end='')
@@ -142,7 +142,7 @@ class Game:
 					# Let player choose a card
 					# Loop until valid choice
 					while True:
-						choice = int(input('\nChoose a card to play (1-{}): '.format(self.gs.hand_size - self.gs.round)))
+						choice = int(input('\n\nChoose a card to play (1-{}): '.format(self.gs.hand_size - self.gs.round)))
 						# Choice out of bounds
 						if choice < 1 or choice > self.gs.hands[self.gs.active_player].size():
 							print('Invalid choice.')
@@ -156,7 +156,7 @@ class Game:
 			played_card = self.gs.hands[self.gs.active_player].deck[choice - 1]
 
 			# Print played card
-			print ('Player {} plays {}.'.format(self.gs.active_player + 1, played_card.to_string()))
+			print ('\nPlayer {} plays {}.'.format(self.gs.active_player + 1, played_card.to_string()))
 
 			# If leading the round
 			if self.gs.turn == 0:
@@ -198,7 +198,7 @@ class Game:
 			# Move on to next player
 			self.gs.next_player()
 
-		print('Player {} takes the trick.'.format(self.gs.taker + 1))
+		print('\nPlayer {} takes the trick.'.format(self.gs.taker + 1))
 
 		# Take trick
 		for card in range(self.gs.num_players):
@@ -232,9 +232,9 @@ class Game:
 		# For each stack of tricks
 		for player in range(self.gs.num_players):
 			# Append 0 to round_score and pips for each player
-
 			round_scores.append(0)
 			pips.append(0)
+
 			# For each card in the stack
 			for card in range(self.gs.tricks[player].size()):
 				current_card = self.gs.tricks[player].deck[card]
@@ -254,8 +254,10 @@ class Game:
 					if current_card.value == 11:
 						jack_taker = player
 
-					# Add pips
-					pips[player] += pip_values[current_card.value]
+				# Add pips
+				pips[player] += pip_values[current_card.value]
+
+			print('Player {} game = {}'.format(player + 1, pips[player]))
 
 		# Find game point taker
 		max_pips = 0
@@ -283,7 +285,7 @@ class Game:
 			self.gs.scores[player] += round_scores[player]
 
 		# Print who got what points
-		print('Player {} took high.'.format(high_taker + 1))
+		print('\nPlayer {} took high.'.format(high_taker + 1))
 		print('Player {} took low.'.format(low_taker + 1))
 		if jack_taker > -1:
 			print('Player {} took jack.'.format(jack_taker + 1))
